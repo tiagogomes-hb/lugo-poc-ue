357400f9a8d4c91ccfb398ec1bd44bcd277f82f0
Tiago Gomes <tiago_gomes@hugoboss.com>
2025-06-12 15:40:08 +0100

    Styling teaser rows

--- blocks/teaser-row/teaser-row.js
+++ blocks/teaser-row/teaser-row.js
@@ -1,7 +1,12 @@
 export default function decorate(block) {
-  const [teaserWrapper] = block.children;
-
-  const blockteaser = document.createElement('blockteaser');
-  blockteaser.textContent = teaserWrapper.textContent.trim();
-  teaserWrapper.replaceChildren(blockteaser);
+  
+  [...block.children].forEach((row) => {
+    const imgoverlay = document.createElement('div');
+    imgoverlay.classList.add('image-overlay');
+    [...row.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'teaser-image';
+      else imgoverlay.append(div)
+    });
+    row.append(imgoverlay);
+  });
 }
