153b7831d4d5f23b76e86b7d59e25d2205782d21
Tiago Gomes <tiago_gomes@hugoboss.com>
2025-06-12 23:01:19 +0100

    Fixing lint issues

--- blocks/teaser-row/teaser-row.js
+++ blocks/teaser-row/teaser-row.js
@@ -1,11 +1,10 @@
 export default function decorate(block) {
-  
   [...block.children].forEach((row) => {
     const imgoverlay = document.createElement('div');
     imgoverlay.classList.add('image-overlay');
     [...row.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'teaser-image';
-      else imgoverlay.append(div)
+      else imgoverlay.append(div);
     });
     row.append(imgoverlay);
   });
